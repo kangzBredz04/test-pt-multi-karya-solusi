4a52e6273f02483325bb6533eb53e2552db333f4
kangzBredz04 <wabredz1234@gmail.com>
2024-10-02 21:18:23 +0700

    feat: enhance App component with new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
-
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <header>ini header</header>
+      <Header />
       <Outlet />
-      <footer>ini footer</footer>
+      <Footer />
     </>
   );
 }
