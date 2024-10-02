6c902572b1c7b76d025712c05c54abaafa192b4b
kangzBredz04 <wabredz1234@gmail.com>
2024-10-02 15:13:19 +0700

    feat: enhance routing configuration with new routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/Login.jsx";
+import UserProfile from "./pages/UserProfile.jsx";
+import CustomerDashboard from "./pages/CustomerDashboard.jsx";
+import EmployeeDashboard from "./pages/EmployeeDashboard.jsx";
+
+const user = {
+  name: "John Doe",
+  email: "john.doe@example.com",
+  bio: "Web Developer with a passion for building beautiful and functional websites.",
+  phone: "123-456-7890",
+  website: "www.johndoe.com",
+  avatar: "https://via.placeholder.com/150",
+};
 
 const router = createBrowserRouter([
   {
@@ -20,9 +32,19 @@ const router = createBrowserRouter([
         title: "Login",
       },
       {
-        path: "/task",
-        // element: <Task />,
+        path: "/user-profile",
+        element: <UserProfile user={user} />,
         title: "Tugas",
+      },
+      {
+        path: "/customer-dashboard",
+        element: <CustomerDashboard />,
+        title: "Customer Dashboard",
+      },
+      {
+        path: "/employee-dashboard",
+        element: <EmployeeDashboard />,
+        title: "Employee Dashboard",
       },
     ],
   },
